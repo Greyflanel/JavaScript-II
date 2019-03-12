a06bf6adb23a6e19e59ff8bcf99d79c083a1a240
Greyflanel <richardlovelace811@gmail.com>
2019-03-12 12:10:16 -0500

    My second commit. I have just finished callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -25,26 +25,51 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 */
 
 
-function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
-}
+// function getLength(arr, cb) {
+//   // getLength passes the length of the array into the callback.
+//     return cb(arr.length);
+// }
+// getLength(items, function(length) {
+//   console.log(length);
+// });
 
-function last(arr, cb) {
-  // last passes the last item of the array into the callback.
-}
 
-function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
-}
+// function last(arr, cb) {
+//   // last passes the last item of the array into the callback.
+//   return cb(arr[arr.length - 1]);
+//   }
+//   last(items, function(lastItem) {
+//   console.log(lastItem);
+//   });
 
-function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
-}
 
-function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
-}
+// function sumNums(x, y, cb) {
+//   //sumNums adds two numbers (x, y) and passes the result to the callback.
+//     return cb(x + y);
+// }
+// sumNums(42, 52, function(sum) {
+//   console.log(sum)
+// });
+
+
+// function multiplyNums(x, y, cb) {
+//   // multiplyNums multiplies two numbers and passes the result to the callback.
+//   return cb(x * y);
+//   }
+//   multiplyNums(85, 63, function(times) {
+//     console.log(times)
+//   });
+
+
+// function contains(item, list, cb) {
+//   // contains checks if an item is present inside of the given array/list.
+//   // Pass true to the callback if it is, otherwise pass false.
+//   return cb(list.includes(item));
+// }
+// contains('purse', items, function(find) {
+// console.log(find)
+// });
+
 
 /* STRETCH PROBLEM */
 
